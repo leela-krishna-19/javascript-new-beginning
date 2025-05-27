96841e9f1ec98032b2a3b0cc26b460c207509f8e
leela-krishna-19 <133492292+leela-krishna-19@users.noreply.github.com>
2025-05-27 06:15:40 +0000

    var added

---
+++ basics/var.js
@@ -0,0 +1,12 @@
+
+const name = "king"  // const value cant be changed
+let mail = "king@123" //we use it
+var pass = "645435"  // we dont use var now
+city = "jaipur" //we need not mention it as variable but it is not good practise
+let state ;//variable is created but not assigned any value
+
+mail = "lee"
+city ="chennai"
+pass = "57655"
+
+console.table([name,mail,pass,city,state])
