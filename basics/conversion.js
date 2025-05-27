febf9a5b5bc0515f1445b89b926fb4e0318bdae6
leela-krishna-19 <133492292+leela-krishna-19@users.noreply.github.com>
2025-05-27 11:50:00 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,7 @@
+
+
+console.log(2>1)  //use comparison operATORS like < , != //returns boolean value
+
+console.log("2" == 2) //it checks value only not data type , string is converted to number
+
+console.log("2"===2)  //it checks datatype also
