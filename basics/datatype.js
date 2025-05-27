bf64fc19c95fd1553cf723c18ec19e0f83a4215c
leela-krishna-19 <133492292+leela-krishna-19@users.noreply.github.com>
2025-05-27 09:07:33 +0000

    datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,11 @@
+
+let string = "lee"
+let number = 57     //datatypes
+let boolean = true
+let state ;
+let unknown = null
+
+console.log(typeof state)
+console.log(typeof null)
+console.log(typeof number)
+console.log(typeof 65) // typeof variablename or value itself
