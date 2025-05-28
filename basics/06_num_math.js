3d7c3c5c3457c255a4d7a06d5c64604b2fda5c32
leela-krishna-19 <133492292+leela-krishna-19@users.noreply.github.com>
2025-05-28 11:28:36 +0000

    math

--- basics/06_num_math.js
+++ basics/06_num_math.js
@@ -14,3 +14,14 @@ console.log(typeof num)
 const rage  =15367700
 console.log(rage.toLocaleString()) // converts to us like currency structure
  
+
+
+//+++++++++++++++++++math+++++++++++
+console.log(Math)
+console.log(Math.abs(-4))  //converts -ve to positive value only
+console.log(Math.round(13.34))
+console.log(Math.floor(13.34))  //gives low 
+console.log(Math.ceil(13.34))  //gives high number
+console.log(Math.min(3,5,3,6))
+console.log(Math.max(3,4,5,3))
+console.log(Math.random()) //random always give value between 0 and 1
